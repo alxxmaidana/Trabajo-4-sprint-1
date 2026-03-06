2264c39ed290d51f66ba8f8265c2ac1cf5f62ba8
Alexander Maidana <maidanaalexanderemanuel@gmail.com>
2026-03-06 09:41:32 -0300

    Agregar Servidor (server.mjs)

---
+++ proyect-root/server.mjs
@@ -0,0 +1,20 @@
+import express from "express";
+
+import {
+    obtenerSuperheroePorIdController,
+    buscarSuperheroePorAtributoController,
+    obtenerSuperheroesMayoresDe30Controller,
+} from "./controllers/superheroeController.mjs";
+
+const app = express();
+const PORT = 3005;
+
+// Rutas
+app.get("/sueperheroes/id/:id", obtenerSuperheroePorIdController);
+app.get("/superheroes/atributo/:atributo/:valor", buscarSuperheroePorAtributoController);
+app.get("/superheroes/edad/mayorA30", obtenerSuperheroesMayoresDe30Controller);
+
+// Lenvantando el servidor en el puerto 3005
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
