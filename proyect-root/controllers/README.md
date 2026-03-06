10db5dce53bd8e18c5d5b3b695489dfcf7159071
Alexander Maidana <maidanaalexanderemanuel@gmail.com>
2026-03-06 08:59:50 -0300

    Actualización del Controlador (controller)

---
+++ proyect-root/controllers/README.md
@@ -0,0 +1,3 @@
+# Controlador
+
+El **Controlador** maneja las solicituda HTTP y utiliza la capa de servicio para obtener los datos necesarios.
---
+++ proyect-root/controllers/superheroeController.mjs
@@ -0,0 +1,40 @@
+import {
+    obtenerSuperheroePorId,
+    buscarSuperheroesPorAtributo,
+    obtenerSuperheroesMayoresDe30,
+} from "../services/superheroeService.mjs";
+
+import {
+    renderizarSuperheroe,
+    renderizarListaSuperHeroes,
+} from "../views/responseView.mjs";
+
+export function obtenerSuperheroePorIdController(required, response) {
+    const { id } = required.params;
+    const superheroe = obtenerSuperheroePorId(parseInt(id));
+
+    if (superheroe) {
+        response.send(renderizarSuperheroe(superheroe));
+    } else {
+        response.status(404).send({ mensaje: "superhéroe no encontrado" });
+    }
+}
+
+export function buscarSuperheroePorAtributoController(required, response) {
+    const { atributo, valor } = required.params;
+    const superheroes = buscarSuperheroesPorAtributo(atributo, valor);
+
+    if (superheroes) {
+        response.send(renderizarListaSuperHeroes(superheroes));
+    } else {
+        response.status(404).send({
+            mensaje: "No se encontraron superhéroer con el atributo",
+            atributo,
+        });
+    }
+}
+
+export function obtenerSuperheroesMayoresDe30Controller(response) {
+    const superheroes = obtenerSuperheroesMayoresDe30();
+    response.send(renderizarListaSuperHeroes(superheroes));
+}
