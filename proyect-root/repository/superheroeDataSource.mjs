70ec7c166aa18bb6f9fd579f6a79af029bac9fae
Alexander Maidana <maidanaalexanderemanuel@gmail.com>
2026-03-05 21:46:39 -0300

    Se subio el proyecto (proyec-root), Estructura del proyecto, RADME.md y actualizacion del la capa de persistencia (repository)

---
+++ proyect-root/repository/superheroeDataSource.mjs
@@ -0,0 +1,6 @@
+export default class SuperHeroeDataSource {
+    // Método abstracto para obrener todos los superheroes
+    obtenerTodos() {
+        throw new Error("Este método debe ser implementado por la subclase");
+    }
+}
---
+++ proyect-root/repository/superheroeRepository.mjs
@@ -0,0 +1,19 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import SuperHeroeDataSource from "./superheroeDataSource.mjs";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default class SuperHerosFileRepository extends SuperHeroeDataSource {
+    constructor() {
+        super();
+        this.filePath = path.join(__dirname);
+    }
+
+    obtenerTodos() {
+        const data = fs.readFileSync(this.filePath, "utf-8");
+        return JSON.parse(data);
+    }
+}
