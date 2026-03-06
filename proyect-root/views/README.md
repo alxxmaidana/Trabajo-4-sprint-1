ad4ddc38f03ce329bbfb4aa51520756495aee98c
Alexander Maidana <maidanaalexanderemanuel@gmail.com>
2026-03-06 09:05:30 -0300

    Actualización de la vista (views)

---
+++ proyect-root/views/README.md
@@ -0,0 +1,3 @@
+# Vista
+
+La **Vista** es responsable de formatear los datos en formato adecuado, en este caso **JSON**.
---
+++ proyect-root/views/responseView.mjs
@@ -0,0 +1,7 @@
+export function renderizarSuperheroe(superheroe) {
+    return JSON.stringify(superheroe, null, 2);
+}
+
+export function renderizarListaSuperHeroes(superheroes) {
+    return JSON.stringify(superheroes, null, 2);
+}
